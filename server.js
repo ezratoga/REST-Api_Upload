ea76c4f3665f21c3d9bcf544c82ed5428e18f02f
ezratoga <ezra.tphtbrt@gmail.com>
2021-07-21 13:06:24 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+global.__basedir = __dirname;
+console.log(__basedir)
+
+var corsOptions = {
+  origin: "http://localhost:5001"
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./src/routes");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let port = 5000;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
